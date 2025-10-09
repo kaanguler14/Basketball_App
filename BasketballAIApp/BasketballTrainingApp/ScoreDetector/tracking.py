10118ec6cdfd4b94431d5b7a4b96e9d3de8db425
glerkaan14 <glerkaan@yahoo.com>
2025-10-10 00:07:15 +0300

    optimize tracking

--- BasketballAIApp/BasketballTrainingApp/ScoreDetector/tracking.py
+++ BasketballAIApp/BasketballTrainingApp/ScoreDetector/tracking.py
@@ -16,6 +16,7 @@ from tkinter import messagebox
 import tkinter as tk
 from tkinter import ttk
 import mediapipe as mp
+import pointSelection as ps
 
 # ------------------------ CONFIG ------------------------
 court_labels = [
@@ -94,79 +95,14 @@ class ShotDetector:
         # --- Homography ---
         ret, first_frame = self.cap.read()
         first_frame = cv2.resize(first_frame, (int(first_frame.shape[1]*scale), int(first_frame.shape[0]*scale)))
-        self.video_points_dict, self.minimap_points_dict = self.load_or_select(first_frame)
+        self.video_points_dict, self.minimap_points_dict = ps.load_or_select(first_frame)
         self.H, self.use_flip, self.h_img = self.compute_homography()
 
         self.shot_history = []  # x,y,made
         self.run()
 
     # ------------------------ HOMOGRAPHY ------------------------
-    def load_or_select(self, video_frame):
-        video_points_dict, minimap_points_dict = {}, {}
 
-        if os.path.exists(vp_json) and os.path.exists(mp_json):
-            if self.ask_reload():
-                video_points_dict = self.select_points(video_frame, "Video")
-                with open(vp_json, "w") as f:
-                    json.dump(video_points_dict, f, indent=2)
-                print("Video noktaları güncellendi.")
-            else:
-                with open(vp_json, "r") as f:
-                    video_points_dict = json.load(f)
-
-            with open(mp_json, "r") as f:
-                minimap_points_dict = json.load(f)
-
-        else:
-            video_points_dict = self.select_points(video_frame, "Video")
-            minimap_points_dict = self.select_points(self.minimap_img, "Minimap")
-            with open(vp_json, "w") as f:
-                json.dump(video_points_dict, f, indent=2)
-            with open(mp_json, "w") as f:
-                json.dump(minimap_points_dict, f, indent=2)
-            print("Noktalar kaydedildi.")
-
-        video_points_dict = {k: (int(v[0]), int(v[1])) for k, v in video_points_dict.items()}
-        minimap_points_dict = {k: (int(v[0]), int(v[1])) for k, v in minimap_points_dict.items()}
-        return video_points_dict, minimap_points_dict
-
-    def ask_reload(self):
-
-        root = tk.Tk()
-        root.withdraw()
-        result = messagebox.askyesno("JSON bulundu", "Önceden kaydedilmiş noktalar bulundu.\nYeniden seçmek ister misin?")
-        root.destroy()
-        return result
-
-    def select_points(self, image, title):
-
-        points_dict = {}
-        def on_mouse(event, x, y, flags, param):
-            if event == cv2.EVENT_LBUTTONDOWN:
-                root = tk.Tk()
-                root.title("Nokta Seçimi")
-                var = tk.StringVar(value=court_labels[0])
-                tk.Label(root, text="Bu nokta hangisi?").pack(padx=10, pady=5)
-                combo = ttk.Combobox(root, values=court_labels, textvariable=var, state="readonly")
-                combo.pack(padx=10, pady=5)
-                tk.Button(root, text="Seç", command=root.quit).pack(pady=5)
-                root.mainloop()
-                label = var.get()
-                root.destroy()
-                points_dict[label] = (int(x), int(y))
-                print(f"{label} seçildi: ({x},{y})")
-        cv2.namedWindow(title)
-        cv2.setMouseCallback(title, on_mouse)
-        while True:
-            tmp = image.copy()
-            for label, pt in points_dict.items():
-                cv2.circle(tmp, pt, 6, (0, 0, 255), -1)
-                cv2.putText(tmp, label, (pt[0]+6, pt[1]-6), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,255), 2)
-            cv2.imshow(title, tmp)
-            if cv2.waitKey(1) & 0xFF == 27:
-                break
-        cv2.destroyWindow(title)
-        return points_dict
 
     def compute_homography(self):
         common_labels = [l for l in court_labels if l in self.video_points_dict and l in self.minimap_points_dict]
